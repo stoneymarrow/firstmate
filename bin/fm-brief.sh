a46d4bc1b5553603579e2cdaaef131800e42d43d
Varun Suchanti <suchantivarun@gmail.com>
2026-07-10 17:25:05 +0530

    no-mistakes(review): Captain, fix brief help and document task budgets

--- bin/fm-brief.sh
+++ bin/fm-brief.sh
@@ -48,7 +48,15 @@ set -eu
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 usage() {
-  sed -n '2,43p' "$0" | sed 's/^# \{0,1\}//'
+  awk '
+    NR == 1 { next }
+    /^#/ {
+      sub(/^# ?/, "")
+      print
+      next
+    }
+    { exit }
+  ' "$0"
 }
 
 case "${1:-}" in
